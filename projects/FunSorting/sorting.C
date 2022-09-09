f398452149ee030ee820a7ce476f961532e0ff63
William.Clymire <William.Clymire@student.allenisd.org>
2022-09-09 15:17:42 -0500

    bucket portion actually works -> still needs merge srot?? maybe idk lol

--- projects/FunSorting/sorting.C
+++ projects/FunSorting/sorting.C
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <cctype>
 using namespace std;
 
 
@@ -12,39 +13,70 @@ using namespace std;
 
 void sortingTime(string arr[], int num)
 {
-  // this makes the alpha dict -> could hardcode for processing benefits
 
-  map<char, int> divNum; //maps are just like dictionaries
-  char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  map<char, int> divNum = {{'a',0}, 
+			   {'b',1},
+			   {'c',2},
+			   {'d',3},
+			   {'e',4},
+			   {'f',5},
+			   {'g',6},
+			   {'h',7},
+			   {'i',8},
+			   {'j',9},
+			   {'k',10},
+			   {'l',11},
+			   {'m',12},
+			   {'n',13},
+			   {'o',14},
+			   {'p',15},
+			   {'q',16},
+			   {'r',17},
+			   {'s',18},
+			   {'t',19},
+			   {'u',20},
+			   {'v',21},
+			   {'w',22,},
+			   {'x',23},
+			   {'y',24},
+			   {'z',25}}; //hardcoded array is .002 seconds faster
+      
+      
   
-  for (int i = 0; i < sizeof(alphabet); i++)
-    divNum.insert({alphabet[i],i});
-
-  vector<string> group[num]; //vector is a dynamic array. There are groups equal to the number of words
+  vector<string> group[26]; //vector is a dynamic array. There are groups equal to the number of words
   int d = 0;
+
   
   for(int i = 0; i < num; i++)
     {
-      char firstChar = arr[i][0]; //gets first char of word
-      d == divNum[firstChar]; //THIS STUPID = MADE ME DEBUG FOR 30+ MINS
+      char firstChar = tolower(arr[i][0]); //gets first char of word
+      d = divNum[firstChar]; //THIS STUPID = MADE ME DEBUG FOR 30+ MINS
       group[d].push_back(arr[i]);
     }
+  
+  for (int i = 0; i < 26; i++)
+    if (group[i].begin() != group[i].end())
+      sort(group[i].begin(), group[i].end()); //can add another sorting method for increased efficiency. Current one thinks capitals are higher
+	
 
-  for (int i = 0; i < num; i++)
-    sort(group[i].begin(), group[i].end()); //can add another sorting method for increased efficiency. Current one thinks capitals are higher
- 
+
   int index = 0;
-  for (int i = 0; i < num; i++)
+  for (int i = 0; i < 26; i++)
     for (int j = 0; j < group[i].size(); j++)
       arr[index++] = group[i][j];
-  
+    
 }
 
 
 int main()
 {
-  string example[] = {"death", "ketamine", "poop", "apples", "bone","Capital", "banana","pee", "bodacious", "come", "adult", "cart", "cant", "darn"};
+  string example[] = {"death", "ketamine", "poop", "apples", "bone","Capital", "banana","pee", "bodacious", "come", "adult", "cart", "can't", "darn"};
   int n = sizeof(example) / sizeof(example[0]);
+
+  for (int i = 0; i < n; i++)
+    transform(example[i].begin(), example[i].end(), example[i].begin(), ::tolower);
+
+
   sortingTime(example, n);
 
 
@@ -54,32 +86,10 @@ int main()
   return 0;
 }
 
-/*
-  map<char, int> arrayNum = {{0,a,}, 
-			     {1,b,},
-			     {2,c,},
-			     {3,d,},
-			     {4,e,},
-			     {5,f,},
-			     {6,g,},
-			     {7,h,},
-			     {8,i,},
-			     {9,j,},
-			     {10,k,},
-			     {11,l,},
-			     {12,m,},
-			     {13,n,},
-			     {14,o,},
-			     {15,p,},
-			     {16,q,},
-			     {17,r,},
-			     {18,s,},
-			     {19,t,},
-			     {18,u,},
-			     {19,v,},
-			     {20,w,},
-			     {21,x,},
-			     {22,y,},
-			     {23,z,}} //hardcoded temporarily -> might actually save processing time tho
-  		  
-*/
+  // this makes the alpha dict -> could hardcode for processing benefits
+
+  //  map<char, int> divNum; //maps are just like dictionaries
+  //char alphabet[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  
+  //for (int i = 0; i < sizeof(alphabet); i++)
+  //  divNum.insert({alphabet[i],i});
